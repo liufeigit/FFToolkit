b723f2fd625d8d2fd15f5a41a5ccaba0115fd322
Forrest Ye <afu@forresty.com>
2013-07-25 18:59:06 +0800

    test pass

--- FFToolkitTests/FFToolkitTests.m
+++ FFToolkitTests/FFToolkitTests.m
@@ -13,20 +13,19 @@
 - (void)setUp
 {
     [super setUp];
-    
+
     // Set-up code here.
 }
 
 - (void)tearDown
 {
     // Tear-down code here.
-    
+
     [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in FFToolkitTests");
+- (void)testExample {
+//    STFail(@"Unit tests are not implemented yet in FFToolkitTests");
 }
 
 @end
